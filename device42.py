983cb9c0852f512338f1e223b99ffde60f5c1969
Omar Sanchez <omar.sanchez@device42.com>
2020-10-23 11:42:34 -0400

    added more print statements, updated libraries. removed hardocded ssl protocol, it will instead just be included in the device42 url. this was mostly done for devs

--- device42.py
+++ device42.py
@@ -1,4 +1,3 @@
-import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -8,6 +7,7 @@ class Device42Doql:
         self.username = config['username']
         self.password = config['password'] if config['password'] else ''
         self.host = config['host']
+        self.verify_ssl = config['verify_ssl']
         self.debug = options['debug']
         self.dry_run = options['dry_run']
 
@@ -17,100 +17,107 @@ class Device42Doql:
         }
 
         try:
-            r = requests.get(url, auth=HTTPBasicAuth(self.username, self.password), headers=headers, verify=False)
+            r = requests.get(url, auth=HTTPBasicAuth(self.username, self.password), headers=headers, verify=self.verify_ssl)
             resp = r.text
+
+            if self.debug == "True":
+                msg1 = 'Status code: %s' % str(r.status_code)
+                msg2 = str(r.text.encode('utf-8'))
+
+                print('\n\t----------- GET FUNCTION -----------')
+                print('\t' + url)
+                print('\t' + msg1)
+                print('\t' + msg2)
+                print('\t------- END OF GET FUNCTION -------\n')
+
         except Exception as e:
             print(e)
             resp = ""
 
-
-        if self.debug:
-            msg1 = 'Status code: %s' % str(r.status_code)
-            msg2 = str(r.text.encode('utf-8'))
-
-            print('\n\t----------- GET FUNCTION -----------')
-            print('\t' + url)
-            print('\t' + msg1)
-            print('\t' + msg2)
-            print('\t------- END OF GET FUNCTION -------\n')
-
         return resp
 
     def get_devices(self):
-        url = 'https://%s/services/data/v1.0/query/?query=SELECT ' \
+        url = '%s/services/data/v1.0/query/?query=SELECT ' \
               'device_pk, name, type, in_service, service_level, virtual_host, ' \
               'hard_disk_size, network_device, ' \
               'asset_no, serial_no, notes, os_version_no, os_fk, hardware_fk ' \
               'FROM view_device_v1&header=yes' % self.host
         msg = '\tGet request to %s ' % url
-        if not self.dry_run:
+        if self.debug == 'True':
+            print("----- Get Devices ----- ")
             print(msg)
         return self._getter(url)
 
     def get_hardware(self, value):
-        url = "https://%s/services/data/v1.0/query/?query=SELECT " \
+        url = "%s/services/data/v1.0/query/?query=SELECT " \
               "name, vendor_fk " \
               "FROM view_hardware_v1 " \
               "WHERE hardware_pk='%s'&header=yes" \
               % (self.host, value)
         msg = '\tGet request to %s ' % url
-        if not self.dry_run:
+        if self.debug == 'True':
+            print("----- Get Hardware ----- ")
             print(msg)
         return self._getter(url)
 
     def get_vendor(self, value):
-        url = "https://%s/services/data/v1.0/query/?query=SELECT " \
+        url = "%s/services/data/v1.0/query/?query=SELECT " \
               "name " \
               "FROM view_vendor_v1 " \
               "WHERE vendor_pk='%s'&header=yes" \
               % (self.host, value)
         msg = '\tGet request to %s ' % url
-        if not self.dry_run:
+        if self.debug == 'True':
+            print("----- Get Vendor ----- ")
             print(msg)
 
         return self._getter(url)
 
     def get_os(self, value):
-        url = "https://%s/services/data/v1.0/query/?query=SELECT " \
+        url = "%s/services/data/v1.0/query/?query=SELECT " \
               "name, vendor_fk " \
               "FROM view_os_v1 " \
               "WHERE os_pk='%s'&header=yes" \
               % (self.host, value)
         msg = '\tGet request to %s ' % url
-        if not self.dry_run:
+        if self.debug == 'True':
+            print("----- Get OS ----- ")
             print(msg)
         return self._getter(url)
 
     def get_subnet(self, value):
-        url = "https://%s/services/data/v1.0/query/?query=SELECT " \
+        url = "%s/services/data/v1.0/query/?query=SELECT " \
               "name " \
               "FROM view_subnet_v1 " \
               "WHERE subnet_pk='%s'&header=yes" \
               % (self.host, value)
         msg = '\tGet request to %s ' % url
-        if not self.dry_run:
+        if self.debug == 'True':
+            print("----- Get Subnet Name ----- ")
             print(msg)
         return self._getter(url)
 
     def get_macs(self, value):
-        url = "https://%s/services/data/v1.0/query/?query=SELECT " \
+        url = "%s/services/data/v1.0/query/?query=SELECT " \
               "hwaddress " \
               "FROM view_netport_v1 " \
               "WHERE device_fk='%s' and hwaddress != ''&header=yes" \
               % (self.host, value)
         msg = '\tGet request to %s ' % url
-        if not self.dry_run:
+        if self.debug == 'True':
+            print("----- Get MAC ----- ")
             print(msg)
         return self._getter(url)
 
     def get_ips(self, value):
-        url = "https://%s/services/data/v1.0/query/?query=SELECT " \
+        url = "%s/services/data/v1.0/query/?query=SELECT " \
               "ip_address, subnet_fk " \
               "FROM view_ipaddress_v1 " \
               "WHERE device_fk='%s'&header=yes" \
               % (self.host, value)
         msg = '\tGet request to %s ' % url
-        if not self.dry_run:
+        if self.debug == 'True':
+            print("----- Get IPS ----- ")
             print(msg)
         return self._getter(url)
 
